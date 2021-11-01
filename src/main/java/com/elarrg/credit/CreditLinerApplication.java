784e2aa4b140f7300d4c79b5aafcba52c8d284a8
Jesus Elias Arriaga Zuniga <JesusElias.ArriagaZuniga@infobip.com>
2021-10-31 19:16:22 -0600

    initial commit

---
+++ src/main/java/com/elarrg/credit/CreditLinerApplication.java
@@ -0,0 +1,13 @@
+package com.elarrg.credit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditLinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditLinerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elarrg/credit/CreditLinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elarrg.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditLinerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
